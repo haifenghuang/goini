dfd2427717899bab221e4058d2b6028ac4592de2
Huang <haifeng.huang@hpe.com>
2017-04-06 15:07:33 +0800

    Initial release

---
+++ README.md
@@ -0,0 +1,36 @@
+# ini
+使用状态机实现的配置文件解析器
+
+## 特性
+1. 简单轻量（API函数只有两个）
+2. 使用状态机实现
+3. 支持全局Section和自定义Section
+
+## API说明
+此解析器提供了两个API函数供用户使用。
+
+### func CfgParse(r io.Reader) error
+对配置文件进行解析
+
+### func CfgGet(section, key string) (out string, err error)
+读取配置文件的信息。  
+参数:  
+    `section`: 块信息，如果为nil，就是被认为是全局Section  
+    `key`:键信息
+    `out`:键对应的值    
+
+## 使用
+配置文件请参照`test.ini`  
+
+## Bug汇报
+如果你发现程序中有任何错误，请发送邮件给我：`fenghai_hhf@163.com`。
+
+## 改进
+由于刚开始学习go语言，对一些go语言的理解还很肤浅，因此写这个代码主要是为了
+练手。对于需要改进的地方，如下：
+1. 使用package ini，而不是package main
+2. 增加测试代码
+3. 链表结构使用go标准库的container/list?
+
+## 许可证
+MIT许可证,详细请参见LICENSE文件
---
+++ ini.go
@@ -0,0 +1,347 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	. "fmt"
+	"io"
+	"os"
+)
+
+//Represent a key-value
+type tagOptions struct {
+	next  *tagOptions //next option
+	name  string      //key
+	value string      //value
+}
+
+//Represent a section
+type tagSections struct {
+	options *tagOptions  //global options(section name is blank)
+	next    *tagSections //next section
+	name    string       //the name of the section
+}
+
+var (
+	gSections    tagSections
+	gLastSection *tagSections
+	gLastOption  *tagOptions
+	keyName      string
+	buffer       string
+	meetSection  bool
+	line         int
+
+	ErrParserError = errors.New("Parser error")
+	ErrMalFormed   = errors.New("Input is malformed, ends inside comment or literal")
+	ErrKeyNotFound = errors.New("key not found")
+)
+
+//FSM ACTION FUNCTIONS --  BEGIN
+func parseError(state int, symbol rune) error {
+	return ErrParserError
+}
+
+func newSection(state int, symbol rune) error {
+	if symbol != '[' {
+		buffer += string(symbol)
+	}
+
+	meetSection = true
+	return nil
+}
+
+func endSection(state int, symbol rune) error {
+	var cur *tagSections
+
+	sec := new(tagSections)
+
+	if gSections.next == nil { /* no section yet */
+		gSections.next = sec
+	} else {
+		cur = &gSections
+		for cur.next != nil {
+			cur = cur.next
+		}
+		cur.next = sec
+	}
+
+	gLastSection = sec
+	gLastSection.name = buffer
+	buffer = ""
+
+	return nil
+}
+
+func newOption(state int, symbol rune) error {
+	keyName += string(symbol)
+
+	return nil
+}
+
+func newValue(state int, symbol rune) error {
+	if keyName != "" {
+		option := new(tagOptions)
+		if meetSection { /* already meet section(s) */
+			if gLastSection.options == nil {
+				gLastSection.options = option
+			} else {
+				gLastOption.next = option
+			}
+		} else {
+			gSections.name = ""           //global section's name is empty
+			if gSections.options == nil { /* no option yet */
+				gSections.options = option
+			} else {
+				cur := gSections.options
+				for cur.next != nil {
+					cur = cur.next
+				}
+				cur.next = option
+			}
+		}
+		gLastOption = option
+
+		gLastOption.name = keyName
+		Printf("newValue, gLastOption.name=[%s]\n", gLastOption.name)
+		keyName = ""
+	}
+	if symbol != '=' {
+		gLastOption.value += string(symbol)
+	}
+
+	/* InStr state, if string contains multiple lines. */
+	if symbol == '\n' {
+		line++
+	}
+
+	return nil
+}
+
+func newLine(state int, symbol rune) error {
+	line++
+	return nil
+}
+
+//FSM ACTION FUNCTIONS --  END
+
+func CfgParse(r io.Reader) error {
+	//Rules for describing the state change and associated actions for the FSM
+	fsm := []struct {
+		state    int  //current state
+		c        rune //current rune read
+		newState int  //next state
+		//FSM action to execute for a certain state
+		action func(state int, symbol rune) error
+	}{
+
+		/* state = [InOptions] - 0*/
+		/* key name could not have space */
+		{0, '\n', 0, newLine},
+		{0, ' ', 0, nil},  /* skip whitespace */
+		{0, '\t', 0, nil}, /* skip whitespace */
+		{0, ';', 1, nil},
+		{0, '#', 1, nil},
+		{0, '=', 4, newValue},
+		{0, '[', 2, newSection},
+		{0, 0, 0, newOption},
+
+		/* state = [Comment] - 1 */
+		{1, '\n', 0, newLine},
+		{1, 0, 1, nil},
+
+		/* state = [InSection] - 2 */
+		/* section name could have space */
+		{2, ']', 3, endSection},
+		//rule{2, ';', 7, nil},      /* section could not contail ';' */
+		//rule{2, '#', 7, nil},      /* section could not contail '#' */
+		{2, 0, 2, newSection}, /* section could contail ';' or '#' */
+
+		/* state = [EndSection] - 3 */
+		{3, ';', 1, nil},
+		{3, '#', 1, nil},
+		{3, 0, 3, nil}, //? "[xxx] yyyy", then yyyy will be ignored
+
+		/* state = [InValue] - 4 */
+		{4, '#', 1, nil},
+		{4, ';', 1, nil},
+		{4, '"', 5, nil},
+		{4, ' ', 4, nil},  /* skip whitespace */
+		{4, '\t', 4, nil}, /* skip whitespace */
+		{4, '\n', 0, newLine},
+		{4, 0, 4, newValue},
+
+		/* state = [InStr] - 5 */
+		/* Note: when string value accross multiple lines,
+		 * then 'line report is not correct.
+		 */
+		{5, '\\', 6, nil},
+		{5, '"', 4, nil},
+		{5, 0, 5, newValue},
+
+		/* state = [StrQuote] - 6 */
+		{6, 0, 5, newValue},
+
+		/* state = [Invalid] - 7 */
+		{7, 0, -1, parseError},
+	}
+	var _ = fsm
+
+	state := 0
+	line = 1
+
+	reader := bufio.NewReader(r)
+
+	var ch rune
+	var err error
+	for {
+		ch, _, err = reader.ReadRune()
+		if err != nil {
+			break
+		}
+
+		for i := 0; i < len(fsm); i++ {
+			currFsm := fsm[i]
+			if currFsm.state == state && (currFsm.c == ch || currFsm.c == 0) {
+				/* state action */
+				if currFsm.action != nil {
+					if actionErr := currFsm.action(state, ch); actionErr != nil {
+						return actionErr
+					}
+				}
+				state = currFsm.newState
+				break
+			} //end if
+		} //inner for
+	} //outer for
+
+	if state != 0 {
+		return ErrMalFormed
+	}
+
+	return nil
+}
+
+/* for debug only */
+func CfgPrint() {
+	var opt *tagOptions
+	var sec *tagSections
+
+	Println("\n==================RESULT:GLOBAL==================")
+
+	if len(gSections.name) == 0 { /* global option */
+		opt = gSections.options
+
+		for opt != nil {
+			Printf("options key[%v], value=[%v]\n", opt.name, opt.value)
+			opt = opt.next
+		} //end for
+	}
+
+	Printf("\n\n==================RESULT:SECTIONS==================")
+	sec = gSections.next
+	for sec != nil {
+		Printf("\nsection name=[%v]\n", sec.name)
+
+		opt = sec.options
+		for opt != nil {
+			Printf("\toptions key[%v], value=[%v]\n", opt.name, opt.value)
+			opt = opt.next
+		} //end inner for
+
+		sec = sec.next
+	} //end for
+}
+
+func CfgGet(section, key string) (out string, err error) {
+	var opt *tagOptions
+	var sec *tagSections
+
+	out = ""
+
+	if len(section) == 0 { /* global options */
+		opt = gSections.options
+
+		for opt != nil {
+			if opt.name == key {
+				//out = opt.value[0:1] + opt.value[1:] //copy string
+				out = opt.value
+				return
+			}
+			opt = opt.next
+		} //end for
+
+		err = ErrKeyNotFound
+		return
+	}
+
+	sec = gSections.next
+	for sec != nil {
+		if sec.name == section {
+			opt = sec.options
+			for opt != nil {
+				if opt.name == key {
+					//out = opt.value[0:1] + opt.value[1:] //copy string
+					out = opt.value
+					return
+				}
+
+				opt = opt.next
+			} //end inner for
+		}
+
+		sec = sec.next
+	} //end for
+
+	return
+}
+
+func main() {
+
+	var out string
+	var err error
+
+	//open file for reading
+	fin, _ := os.Open("./test.ini")
+	defer fin.Close()
+
+	if err = CfgParse(fin); err != nil {
+		Printf("Error:%s", err.Error())
+		return
+	}
+
+	Println("\n==================GET RESULT:GLOBAL==================")
+	out, _ = CfgGet("", "aa")
+	Printf("Global section, aa=[%v]\n", out)
+
+	out, _ = CfgGet("", "a")
+	Printf("Global section, a=[%v]\n", out)
+
+	out, _ = CfgGet("", "b")
+	Printf("Global section, b=[%v]\n", out)
+
+	out, _ = CfgGet("", "c")
+	Printf("Global section, c=[%v]\n", out)
+
+	Println("\n==================GET RESULT:[ab;cdefg]==================")
+	out, _ = CfgGet("ab;cdefg", "c")
+	Printf("Named section[ab;cdefg], c=[%v]\n", out)
+
+	out, _ = CfgGet("ab;cdefg", "d")
+	Printf("Named section[ab;cdefg], d=[%v]\n", out)
+
+	out, _ = CfgGet("ab;cdefg", "e")
+	Printf("Named section[ab;cdefg], e=[%v]\n", out)
+
+	Println("\n==================GET RESULT:[xxxx]==================")
+	out, _ = CfgGet("xxxx", "e")
+	Printf("Named section[xxxx], e=[%v]\n", out)
+
+	out, _ = CfgGet("xxxx", "m")
+	Printf("Named section[xxxx], m=[%v]\n", out)
+
+	out, _ = CfgGet("xxxx", "n")
+	Printf("Named section[xxxx], n=[%v]\n", out)
+
+	Println("\n==================[DEBUG]==================\n")
+	CfgPrint()
+}
